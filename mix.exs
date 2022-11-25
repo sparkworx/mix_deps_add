974bbbb57766bce3b88ceb3d638c0bce93e0123d
Ian Woodbury <sparkworx@gmail.com>
2022-11-25 16:01:31 -0700

    add code formatter support

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule MixDepsAdd.Mixfile do
   def project do
     [app: :mix_deps_add,
      version: "0.1.3",
-     elixir: "~> 1.4",
+     elixir: "~> 1.6",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod]
   end
