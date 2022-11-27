aada5b5b3b04ac96ad59066d2b2e00d61c382cb0
Ian Woodbury <sparkworx@gmail.com>
2022-11-26 22:01:31 -0700

    sync `mix.exs` boilerplate with contemporary Elixir `mix new`
    * while not deprecated, any form of `build_embedded:` is not required
      for correct functioning.

--- mix.exs
+++ mix.exs
@@ -1,4 +1,4 @@
-defmodule MixDepsAdd.Mixfile do
+defmodule MixDepsAdd.MixProject do
   use Mix.Project
 
   def project do
@@ -6,17 +6,17 @@ defmodule MixDepsAdd.Mixfile do
       app: :mix_deps_add,
       version: "0.1.3",
       elixir: "~> 1.6",
-      build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod
     ]
   end
 
   # Configuration for the OTP application
   #
-  # Type "mix help compile.app" for more information
+  # Run "mix help compile.app" to learn about applications.
   def application do
-    # Specify extra applications you'll use from Erlang/Elixir
-    [extra_applications: [:logger, :inets, :ssl]]
+    [
+      extra_applications: [:logger, :inets, :ssl]
+    ]
   end
 
   # Note that we don't have a deps/0 function here; as a Mix task, we can't
